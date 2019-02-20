0d1f2557c06853231056b9a03613e29060077d97
Bryan Mutai <mutaiwork@gmail.com>
2019-02-20 16:59:53 +0300

    Initial year navigation bar setup

--- components/YearNavigator.js
+++ components/YearNavigator.js
@@ -25,11 +25,15 @@ export default class YearNavigator extends React.Component {
     const years = this.props.years;
     return (
       <ul className="year-list">
-        <li> About</li>
+        <li>
+          <a> About</a>
+        </li>
         {years && years.length > 0 ? (
           years.map(year => (
             //   <Year info={year} />
-            <li>{year}</li>
+            <li>
+              <a>{year}</a>
+            </li>
           ))
         ) : (
           <p>Error loading the years</p>
@@ -38,6 +42,26 @@ export default class YearNavigator extends React.Component {
           .year-list {
             display: grid;
             list-style-type: none;
+            padding: 0;
+          }
+          li a {
+            display: inline-block;
+            text-decoration: none;
+            width: 100%;
+            background-color: #dadad9;
+            color: black;
+            text-align: center;
+            border-width: 1pt;
+            border-style: solid;
+          }
+
+          li a:focus,
+          li a:hover,
+          li a:active {
+            background-color: #ececec;
+            color: #ff00ff;
+            border-width: 8pt;
+            width: 95%;
           }
         `}</style>
       </ul>
